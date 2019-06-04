61465245af0baff4ba0f6ae177944039f3ee2424
Martin Sehnoutka <msehnout@redhat.com>
2019-06-04 13:36:40 +0200

    migrate to openssl

--- rlibprd/src/email2domain.rs
+++ rlibprd/src/email2domain.rs
@@ -1,14 +1,14 @@
 use data_encoding::HEXLOWER;
-use sha2::{Digest, Sha256};
+use openssl::sha;
 
 pub fn email2domain(email_address: &str) -> Result<String, &'static str> {
     let split: Vec<_> = email_address.split('@').collect();
     if split.len() == 2 {
         let local = split[0];
         let domain = split[1];
-        let mut hasher = Sha256::new();
-        hasher.input(local.as_bytes());
-        let digest = hasher.result();
+        let mut hasher = sha::Sha256::new();
+        hasher.update(local.as_bytes());
+        let digest = hasher.finish();
         let digest_str = HEXLOWER.encode(&digest[0..28]);
         Ok(format!("{}._openpgpkey.{}", &digest_str, domain))
     } else {
