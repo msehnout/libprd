14b1f65eaacdbb996111eb2a953fcf4f9daecfce
Martin Sehnoutka <msehnout@redhat.com>
2019-06-04 13:36:56 +0200

    improve public api

--- rlibprd/src/validator.rs
+++ rlibprd/src/validator.rs
@@ -1,4 +1,6 @@
+use crate::email2domain::*;
 use crate::unbound::*;
+use crate::{KeyInfo, Validity};
 use data_encoding::BASE64;
 use std::ptr;
 use std::os::raw::{c_char, c_int};
@@ -23,9 +25,13 @@ impl Validator {
         })
     }
 
-    pub fn resolve(&mut self, domain: &str) {
+    pub fn validate(&mut self, key_info: KeyInfo) -> Validity {
         let mut result: *mut ub_result = ptr::null_mut();
-        let domain = String::from(domain) + "\0";
+        let domain = match email2domain(&key_info.email) {
+            Ok(d) => d,
+            Err(_) => return Validity::ERROR,
+        };
+        let domain = domain + "\0";
         unsafe {
             let retval = ub_resolve(self.unbound_context,
                                     domain.as_ptr() as *const i8,
@@ -34,40 +40,36 @@ impl Validator {
                                     &mut result);
 
             if retval != 0 {
-                eprintln!("Failed to resolve host");
-                return;
+                return Validity::ERROR;
             }
-
+            if (*result).bogus != 0 {
+                return Validity::BOGUS_RESULT;
+            }
+            if (*result).secure == 0 {
+                return Validity::RESULT_NOT_SECURE;
+            }
             if (*result).nxdomain != 0 {
-                eprintln!("NXDOMAIN");
-                return;
+                return Validity::PROVEN_NONEXISTENCE;
             }
-
-            if (*result).secure != 0 {
-                eprintln!("SECURE");
-            } else {
-                eprintln!("UNSECURE");
-            }
-
             if (*result).havedata == 0 {
-                eprintln!("The result does not contain data");
+                return Validity::ERROR;
             } else {
-                unsafe fn from_buf_raw(ptr: *const c_char, elts: usize) -> Vec<u8> {
-                    let reinterpreted_ptr = ptr as *const u8;
-                    let mut dst = Vec::with_capacity(elts);
-                    dst.set_len(elts);
-                    ptr::copy(reinterpreted_ptr, dst.as_mut_ptr(), elts);
-                    dst
-                }
-
                 let key_from_dns: *const c_char = *((*result).data.offset(0));
                 let key_length: usize = *((*result).len.offset(0)) as usize;
-                let mut key_vector = from_buf_raw(key_from_dns, key_length);
+
+                let reinterpreted_ptr = key_from_dns as *const u8;
+                let mut key_vector = Vec::with_capacity(key_length);
+                key_vector.set_len(key_length);
+                ptr::copy(reinterpreted_ptr, key_vector.as_mut_ptr(), key_length);
+
                 let key_str = BASE64.encode(&key_vector);
                 println!("KEY: {}, len: {}", key_str, key_length);
+                if key_str == key_info.b64_key {
+                    return Validity::VALID;
+                } else {
+                    return Validity::REVOKED;
+                }
             }
-
-            println!("end!");
         }
     }
 }
